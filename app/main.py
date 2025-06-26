ff0c7842a04d7e607a4b3fe83f72b1edb817dc7d
jgcha <sean9887@github.com>
2025-06-26 16:56:44 +0900

    add dog images

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ async def root():
         <head><title>ArgoCD CI/CD Demo</title></head>
         <body>
             <h1>Hello from CI/CD!</h1>
-            <img src="/image/image_1.png" width="300px" />
+            <img src="/image/dog1.png" width="300px" />
         </body>
     </html>
     """
